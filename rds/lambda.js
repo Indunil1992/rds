ae97daf5f5aeb4ca1d3f4622e7fa4657c1241ed3
Indunil1992 <indunil@adroitlogic.com>
2019-11-27 10:51:20 +0530

    1

--- rds/lambda.js
+++ rds/lambda.js
@@ -4,24 +4,24 @@ const rds = new SL_AWS.RDS(connectionManager);
 
 exports.handler = function (event, context, callback) {
 
-    // You can pass the existing connection to this function.
-    // A new connection will be created if it's not present as the third param 
-    // You must always end/destroy the DB connection after it's used
-    rds.query({
-        instanceIdentifier: 'indunil1',
-        query: 'name, age',
-        inserts: ["indunil1", 27]
-    }, function (error, results, connection) {
-        if (error) {
-            console.log("Error occurred");
-            throw error;
-        } else {
-            console.log("Success")
-            console.log(results);
-        }
+    // // You can pass the existing connection to this function.
+    // // A new connection will be created if it's not present as the third param 
+    // // You must always end/destroy the DB connection after it's used
+    // rds.query({
+    //     instanceIdentifier: 'indunil1',
+    //     query: 'name, age',
+    //     inserts: ["indunil1", 27]
+    // }, function (error, results, connection) {
+    //     if (error) {
+    //         console.log("Error occurred");
+    //         throw error;
+    //     } else {
+    //         console.log("Success")
+    //         console.log(results);
+    //     }
 
-        connection.end();
-    });
+    //     connection.end();
+    // });
 
 
     // You must always end/destroy the DB connection after it's used
